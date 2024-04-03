398f3c908720e0a99fb81d026db9806942ac206b
tkhshsh <xlxolxus@gmail.com>
2024-04-03 22:34:47 +0900

    kadai_010を追加

---
+++ kadai_010/summary.js
@@ -0,0 +1,18 @@
+//function関数で関数内にある色々な処理を行うよ
+$(function(){
+    //change-colorというIDをHTMLから取得し、クリックすると
+    $('#change-color').on('click',function(){
+        //targetというIDであるこんにちは！の色をピンクにするよ
+        $('#target').css('color','pink');
+    });
+    $('#change-text').on('click',function(){
+        //textでHello！に変更するよ
+        $('#target').text('Hello!')
+    });
+    $('#fade-out').on('click',function(){
+        $('#target').fadeOut();
+    });
+    $('#fade-in').on('click',function(){
+        $('#target').fadeIn();
+    });
+});
